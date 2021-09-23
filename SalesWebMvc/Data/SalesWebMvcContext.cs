a4900338116915aeb77f6048007eade2cb454b5e
mcpedroni <marcelcontri@gmail.com>
2021-09-23 13:33:16 +0930

    Second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -5,15 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 
-namespace SalesWebMvc.Data
-{
-    public class SalesWebMvcContext : DbContext
-    {
-        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
-            : base(options)
-        {
+namespace SalesWebMvc.Data {
+    public class SalesWebMvcContext : DbContext {
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
+            : base(options) {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,22 @@
-﻿
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
 
 namespace SalesWebMvc.Models {
     public class Department {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department() {  }
+        public Department(int id, string name) {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller s) {
+            Sellers.Add(s);
+        }
+        public double TotalSales(DateTime inital, DateTime final) {
+            return Sellers.Sum(seller => seller.TotalSales(inital, final));
+        }
     }
 }
