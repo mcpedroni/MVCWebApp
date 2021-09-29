91a77ae88725e7c90bb4a19ae0533c3fa5797947
mcpedroni <marcelcontri@gmail.com>
2021-09-29 14:32:53 +0930

    Create form Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,20 @@ namespace SalesWebMvc.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll(); 
             return View(list);
+        }
+
+        public IActionResult Create() {
+            
+            return View();
+        }        
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken] //previne que alguem aproveite a sessao de autenticacao e envia dados maliciosos.
+        //insert seller on database
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            //retorna pra tela Index
+            return RedirectToAction(nameof(Index));
         }
     }
 }
